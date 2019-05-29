6f07b800a4d9fcd6e6fb22293bfd023336440f8c
saibimajdi <saibimajdi@outlook.com>
2019-05-28 18:34:16 -0600

    Edmug first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Edmug.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
